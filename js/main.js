86508c268a5a485ed6793963232b7fe152a7380b
alislahprahim <alislahiprahim99@gmail.com>
2023-12-24 01:31:55 +0200

    working on register stepper

--- js/main.js
+++ js/main.js
@@ -5,95 +5,71 @@ function scroll_to_class(element_class, removed_height) {
   }
 }
 
-function bar_progress(progress_line_object, direction) {
-  var number_of_steps = progress_line_object.data("number-of-steps");
-  var now_value = progress_line_object.data("now-value");
-  var new_value = 0;
-  if (direction == "right") {
-    new_value = (now_value + 100) / number_of_steps;
-    console.log("new_value", now_value, number_of_steps);
-    console.log("new_value", new_value);
-  } else if (direction == "left") {
-    new_value = now_value - 100 / number_of_steps;
-  }
-  progress_line_object
-    .attr("style", "width: " + new_value + "%;")
-    .data("now-value", new_value);
-}
-
 jQuery(document).ready(function () {
   const loader = document.querySelector(".loader");
   // Hide the loader once the content is fully loaded
   window.onload = function () {
     loader.style.display = "none";
   };
-  /*
-              Form
-          */
+
   $(".f1 fieldset:first").fadeIn("fast");
 
-  // $('.f1 input[type="text"], .f1 input[type="password"], .f1 textarea').on('focus', function () {
-  //     $(this).removeClass('input-error');
-  // });
-
-  // next step
+  // forwards
   $(".f1 #btn-next").on("click", function () {
-    var parent_fieldset = $(this).parents("fieldset");
-    var next_step = true;
-    // navigation steps / progress steps
     var current_active_step = $(this).parents(".f1").find(".f1-step.active");
+    var currentStep = $(this).closest("fieldset").data("step");
+    var nextStep = parseInt(currentStep) + 1;
     var progress_line = $(this).parents(".f1").find(".f1-progress-line");
-
-    // fields validation
-    // parent_fieldset.find('input[type="text"], input[type="password"], textarea').each(function () {
-    //     if ($(this).val() == "") {
-    //         $(this).addClass('input-error');
-    //         next_step = false;
-    //     }
-    //     else {
-    //         $(this).removeClass('input-error');
-    //     }
-    // });
-    // fields validation
-
-    if (next_step) {
-      parent_fieldset.fadeOut(400, function () {
-        // change icons
+    // Hide current step
+    $(this)
+      .closest("fieldset")
+      .fadeOut(400, function () {
+        updateProgress(progress_line, nextStep - 1);
         current_active_step
           .removeClass("active")
           .addClass("activated")
           .next()
           .addClass("active");
-        // progress bar
-        bar_progress(progress_line, "right");
-        // show next step
-        $(this).next().fadeIn("fast");
-        // scroll window to beginning of the form
+        $(`.f1 fieldset[data-step="${nextStep}"]`).fadeIn("fast");
         scroll_to_class($(".f1"), 20);
       });
-    }
   });
 
-  // previous step
+  // backwards
   $(".f1 .btn-previous").on("click", function () {
-    // navigation steps / progress steps
     var current_active_step = $(this).parents(".f1").find(".f1-step.active");
+    var currentStep = $(this).closest("fieldset").data("step");
+    var prevStep = parseInt(currentStep) - 1;
     var progress_line = $(this).parents(".f1").find(".f1-progress-line");
 
+    // Hide current step
     $(this)
-      .parents("fieldset")
+      .closest("fieldset")
       .fadeOut(400, function () {
-        // change icons
-        current_active_step
-          .removeClass("active")
-          .prev()
-          .removeClass("activated")
-          .addClass("active");
-        // progress bar
-        bar_progress(progress_line, "left");
-        // show previous step
-        $(this).prev().fadeIn();
-        // scroll window to beginning of the form
+        updateProgress(progress_line, prevStep - 1);
+        current_active_step.removeClass("active").prev().addClass("active");
+        $(`.f1 fieldset[data-step="${prevStep}"]`).fadeIn("fast");
+        scroll_to_class($(".f1"), 20);
+      });
+  });
+
+  // steps navigation
+  $(".f1 .f1-step").on("click", function () {
+    var progress_line = $(this).parents(".f1").find(".f1-progress-line");
+    var targetStep = $(this);
+    var targetStepIndex = $(this).index();
+
+    // Fade out all steps except the target step
+    $(".f1 fieldset").not(`[data-step="${targetStepIndex}"]`).fadeOut(400);
+
+    $(".f1 fieldset[data-step]")
+      .promise()
+      .done(function () {
+        targetStep.prevAll(".f1-step").addClass("active");
+        targetStep.nextAll(".f1-step").removeClass("active");
+        targetStep.addClass("active");
+        updateProgress(progress_line, targetStepIndex - 1);
+        $(`.f1 fieldset[data-step="${targetStepIndex}"]`).fadeIn("fast");
         scroll_to_class($(".f1"), 20);
       });
   });
@@ -112,4 +88,12 @@ jQuery(document).ready(function () {
     // });
     // fields validation
   });
+
+  // progress bar updates
+  function updateProgress(progress_line, stepNumber) {
+    var ratio = progress_line.data("ratio");
+    progress_line
+      .attr("style", "width: " + ratio * stepNumber + "%;")
+      .data("ratio", ratio);
+  }
 });
